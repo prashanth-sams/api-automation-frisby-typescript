7529fa16474435e9c88c0142714457bdce672f3f
prashanth-sams <sams.prashanth@gmail.com>
2019-02-14 19:19:14 +0400

    clean archives

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,11 +1,17 @@
 module.exports = function(grunt) {
   grunt.initConfig({
+    clean : {
+      output: './build/*'
+    },
     ts: {
       default : {
         tsconfig: './tsconfig.json'
       }
     }
   });
+
+  grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks("grunt-ts");
-  grunt.registerTask("default", ["ts"]);
+
+  grunt.registerTask('default', ['clean', 'ts']);
 };
